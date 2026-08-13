8497f8488ffb764ce920cf77f739ded5325f0eea
MrTorres30 <torresandelizw@gmail.com>
2026-08-13 19:41:48 -0400

    feat: implementar patron repositorio para usuarios

---
+++ GastosPersonales.Domain/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,16 @@
+﻿using GastosPersonales.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GastosPersonales.Domain.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario?> GetUsuarioAsync(int id);
+        Task<Usuario?> GetByEmailAsync(string email);
+        Task AddAsync(Usuario usuario);
+        Task UpdateAsync(Usuario usuario);
+
+    }
+}
---
+++ GastosPersonales.Infrastructure/Repositories/UsuarioRepository.cs
@@ -0,0 +1,40 @@
+﻿using GastosPersonales.Domain.Entities;
+using GastosPersonales.Domain.Repositories;
+using GastosPersonales.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GastosPersonales.Infrastructure.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsuarioRepository(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<Usuario?> GetUsuarioAsync(int id)
+        {
+            return await _context.Usuarios.FindAsync(id);
+        }
+
+        public async Task<Usuario?> GetByEmailAsync(string email)
+        {
+            return await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task AddAsync(Usuario usuario)
+        {
+            await _context.Usuarios.AddAsync(usuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Usuario usuario)
+        {
+            _context.Usuarios.Update(usuario);
+            await _context.SaveChangesAsync(); 
+        }
+    }
+}
